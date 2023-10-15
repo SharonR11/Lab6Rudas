b298a076fc90553270cb3aff9cb1de0cce8ff05c
SharonR11 <sharon.rudas@tecsup.edu.pe>
2023-10-14 19:28:23 -0500

    tarea

--- Lab6Rudas/Lab6Rudas/Estudiante.cs
+++ Lab6Rudas/Lab6Rudas/Estudiante.cs
@@ -5,37 +5,25 @@ using System.Text;
 
 namespace Lab6Rudas
 {
-    public class Estudiante:List<ListViewGroup>
+    public class Estudiante
     {
         public string Seccion { get; set; }
         public string Nombres { get; set; }
         public string Apellidos { get; set; }
-        private Estudiante( string nombres, String apellidos)
+
+        public Estudiante(string seccion, string nombres, string apellidos)
         {
+            Seccion = seccion;
             Nombres = nombres;
-            Apellidos= apellidos;
+            Apellidos = apellidos;
         }
-        public static IList<Estudiante> All { private set; get; }
+        public string Persona => $"{Nombres} {Apellidos}";
 
-        static Estudiante()
-        {
-            List<Estudiante> Groups = new List<Estudiante> {
 
-            new Estudiante ("Seccion A", "A"){
-
-                new ListViewGroup("Sharon", "Rudas", new switchCellPage(),""),
-                new ListViewGroup("Deysi", "Lloja", new switchCellPage(), "grapefruit.jpg"),
-                new ListViewGroup("Ana", "Cristina", new switchCellPage(), "grapefruit.jpg"),
-                new ListViewGroup("Luis", "ISlachin", new switchCellPage(), "grapefruit.jpg")
-            },
-            new Estudiante ("Seccion B", "B"){
-                new ListViewGroup("Brooke", "Lumia", new switchCellPage(),""),
-                new ListViewGroup("Bobby", "Xperia", new switchCellPage(), "grapefruit.jpg"),
-                new ListViewGroup("Bella", "Desire", new switchCellPage(), "grapefruit.jpg"),
-                new ListViewGroup("Ben", "Chocolate", new switchCellPage(), "grapefruit.jpg")
-            }
-        };
-            All = Groups;
-        }
     }
+
+
 }
+
+
+
--- Lab6Rudas/Lab6Rudas/ListViewGroup.xaml.cs
+++ Lab6Rudas/Lab6Rudas/ListViewGroup.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,12 +11,57 @@ using Xamarin.Forms.Xaml;
 namespace Lab6Rudas
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    
+
     public partial class ListViewGroup : ContentPage
     {
+
+
+        public class Grupo<TKey, TItem> : List<TItem>
+        {
+            public TKey Key { get; private set; }
+
+            public Grupo(TKey key, IEnumerable<TItem> items)
+            {
+                Key = key;
+                AddRange(items);
+            }
+        }
+
+        ObservableCollection<Grupo<string, Estudiante>> estudiantesGrupos = new ObservableCollection<Grupo<string, Estudiante>>();
+
+        public ObservableCollection<Grupo<string, Estudiante>> EstudiantesGrupos { get { return estudiantesGrupos; } }
+
+
+
         public ListViewGroup()
         {
             InitializeComponent();
-            
+
+            var estudiantes = new List<Estudiante>
+            {
+                new Estudiante("Seccion A", "Sharon", "Rudas"),
+                new Estudiante("Seccion A", "Deysi", "Lloja"),
+                new Estudiante("Seccion A", "Ana", "Cristina"),
+                new Estudiante("Seccion A", "Luis", "Islachin"),
+                new Estudiante("Seccion B", "Brooke", "Lumia"),
+                new Estudiante("Seccion B", "Bobby", "Xperia"),
+                new Estudiante("Seccion B", "Bella", "Desire"),
+                new Estudiante("Seccion B", "Ben", "Chocolate")
+            };
+
+            var grupos = estudiantes
+                .GroupBy(e => e.Seccion)
+                .Select(grupo => new Grupo<string, Estudiante>(grupo.Key, grupo));
+
+            estudiantesGrupos = new ObservableCollection<Grupo<string, Estudiante>>(grupos);
+
+            GroupedView.ItemsSource = estudiantesGrupos;
+
+            GroupedView.ItemsSource = grupos;
+
+
         }
 
     }
